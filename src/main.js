fa9c81e1c6837180992c3598d07fcf2e0919c291
edacevedo <edinsonacevedoc@gmail.com>
2022-03-21 21:01:20 -0500

    Adding shading

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-window.onload = () => {
+window.onload = () => {  
+    paintDash(16);
+    document.querySelector('#clear-btn').addEventListener('click', onClear);
+}
+
+let paintDash = (gridSize) => {
     let mainContainer = document.querySelector('#main-container'),
-        gridSize = 16,
         childItem,
         minWidth = Math.round(100/gridSize);
     
-    for (let index = 0; index < gridSize*gridSize; index++) {
+        mainContainer.innerHTML = '';
+
+    for (let index = 1; index < gridSize*gridSize; index++) {
         childItem = document.createElement('div');
         childItem.style.minWidth = `${minWidth}%`;
         childItem.addEventListener('mouseover', onMouseOverChildItem);
@@ -13,7 +19,24 @@ window.onload = () => {
 }
 
 let onMouseOverChildItem = (event) => {
-    let childItem = event.target;
-    
-    childItem.style.backgroundColor = 'black';
+    let childItem = event.target,
+        currentTone = childItem.currentTone;
+
+    currentTone = currentTone === 0 ? currentTone : (currentTone || 220) - 20;
+    childItem.style.backgroundColor = `RGB(${currentTone}, ${currentTone}, ${currentTone})`;
+    childItem.currentTone = currentTone;
 };
+
+let onClear = () => {
+    let gridSize = window.prompt('Which will be the panel size? (e.g. 16)');
+    
+    gridSize = Number.parseInt(gridSize);
+
+    if (gridSize < 16 || gridSize > 100) {
+        gridSize = 32
+    }
+
+    if (gridSize) {
+        paintDash(gridSize);
+    }
+}
